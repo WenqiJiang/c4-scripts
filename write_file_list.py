479c75da62ae8e6f0e504ffc3602369f3cb06e1c
wenqijiang <wenqi.j@columbia.edu>
2022-05-16 08:55:32 +0200

    update local url conversion

--- write_file_list.py
+++ write_file_list.py
@@ -1,19 +1,21 @@
-file_path_in_list_path = './bloom_filter_input_file_list'
-#file_path_in_list_path = './json_input_file_list'
+#file_path_in_list_path = './bloom_filter_input_file_list'
+file_path_in_list_path = './json_input_file_list'
 
-file_path_out_list_path = './bloom_filter_output_file_list'
+#file_path_out_list_path = '/mnt/scratch/wenqi/c4/multilingual_url'
 #file_path_out_list_path = './passage_output_file_list'
+file_path_out_list_path = './url_output_file_list'
 
 with open(file_path_in_list_path, 'w') as f:
     lines = []
-    for i in range(4096):
+    for i in range(11264):
         # lines.append('/mnt/scratch/wenqi/c4/multilingual/c4-en.tfrecord-{}-of-11264.json'.format(str(i).zfill(5)) + '\n')
-        lines.append('/mnt/scratch/wenqi/c4-passages/multilingual/c4-en.tfrecord-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
+        #lines.append('/mnt/scratch/wenqi/c4-passages/multilingual/c4-en.tfrecord-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
+        lines.append('/mnt/scratch/wenqi/c4/multilingual/c4-en.tfrecord-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
     f.writelines(lines)
 
 with open(file_path_out_list_path, 'w') as f:
     lines = []
-    for i in range(4096):
-        lines.append('/mnt/scratch/wenqi/c4_deduplicated/multilingual_passages/c4-en.dedup_passages-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
+    for i in range(11264):
+        lines.append('/mnt/scratch/wenqi/c4/multilingual_url/c4-en.url-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
         # lines.append('/mnt/scratch/wenqi/c4-passages/multilingual/c4-en.tfrecord-{}-of-11264.json'.format(str(i).zfill(5)) + '\n' )
     f.writelines(lines)
