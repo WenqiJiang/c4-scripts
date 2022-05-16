244a32034a1ef6da600866d4d28085b4182453bf
wenqijiang <wenqi.j@columbia.edu>
2022-05-16 06:38:48 +0000

    update url

---
+++ download_GS/write_file_list_tmp.py
@@ -0,0 +1,23 @@
+file_path_in_list_path = './url_list'
+
+with open(file_path_in_list_path, 'w') as f:
+    lines = []
+    parent_url = 'https://storage.googleapis.com/c4multilingual201918/tensorflow_datasets/c4_json_url/multilingual201918nodedup/3.0.1/'
+
+    for i in range(2048):
+        lines.append(parent_url + 'c4-en.tfrecord-{}-of-01024.json'.format(str(i).zfill(5)) + '\n' )
+
+    for i in range(1024):
+        lines.append(parent_url + 'c4-de.tfrecord-{}-of-01024.json'.format(str(i).zfill(5)) + '\n' )
+
+    for i in range(1024):
+        lines.append(parent_url + 'c4-ru.tfrecord-{}-of-01024.json'.format(str(i).zfill(5)) + '\n' )
+
+    for i in range(32):
+        lines.append(parent_url + 'c4-ja.tfrecord-{}-of-01024.json'.format(str(i).zfill(5)) + '\n' )
+
+    for i in range(4):
+        lines.append(parent_url + 'c4-zh.tfrecord-{}-of-01024.json'.format(str(i).zfill(5)) + '\n' )
+
+    f.writelines(lines)
+
